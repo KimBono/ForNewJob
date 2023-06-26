e92b306273231cc3097c33a62d6e7e81cece5197
Kimbono <swk4774@gmail.com>
2023-06-26 21:46:11 +0900

    feature: Jetpack Compose 기초 7 (Compose에서의 상태)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +30,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    MyApp(names = listOf("A","AAA"))
                 }
             }
         }
@@ -30,17 +39,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun Greeting(name: String) {
+    var expanded = remember { mutableStateOf(false) }
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value  }
+            ) {
+                Text( if(expanded.value) "Show less" else  "Show more")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names : List<String> = listOf("World","Compose"))
+{
+    Column(modifier){
+        for(name in names){
+            Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true,widthDp = 320)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        MyApp(names = listOf("A","AAA"))
     }
 }
